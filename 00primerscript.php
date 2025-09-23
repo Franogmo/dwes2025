dcf7b8bfa0fd1046468cef9db9b9e5bb1b7c91e7
Franogmo <franogmo13@gmail.com>
2025-09-23 19:46:32 +0200

    Commit 1

---
+++ 00primerscript.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>primer script en php</title>
+    </head>
+    <body>
+        <h1>primer script en php</h1>
+        <?php
+        echo "<h3>IES Gran Capitán</h3>";
+        echo "<p>Tonto el que lo hackee</p>";
+        ?>
+        &copy; El fran
+    </body>
+</html>
---
+++ ra2/01elementos_lenguaje.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>segundo script en php. Elementos del lenguaje.</title>
+    </head>
+    <body>
+        <h1>Segundo script en php. Elementos del lenguaje.</h1>
+        <h2>Entrada y salida</h2>
+        <p>La entrada de datos en php es con un formulario o enlace.La salida siempre se produce con la función echo, y su forma abreviada, 
+            y la función print.</p>
+        <p>Además tenemos la función printf para salida con formato.</p>
+        <h3>Función echo</h3>
+        <?php 
+        echo "<p>La función echo emite el resultado de una expresión a la salida (del servidor al cliente web). Se puede usar como función 
+        o como construcción del lenguaje (o sea, sin paréntesis)</p>";
+        echo "<p>Esto es un párrafo HTML enviado con echo.</p>";
+        ?> 
+        &copy; El fran
+    </body>
+</html>
