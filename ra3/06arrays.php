f713109e54fa60e8eef696e0f3cb3946fd849dff
Franogmo <franogmo13@gmail.com>
2025-10-09 16:02:28 +0200

    arrays2

--- ra3/06arrays.php
+++ ra3/06arrays.php
@@ -34,5 +34,41 @@
         <p>Se buscan elementos concretos dentro de estos con 2 corchetes.</p> 
         <p>$tablafutbolistas[1]['nombre'];  Buscaría el nombre del segundo futbolista.</p>
         <p>borrar<p>
+        <h2>Tipos de bucle para recorrer un array</h2>
+        <p>For tradicional (no se usa apenas)</p>
+        <p>while (especialmente para arrays escalares)</p>
+        <h3>Bucle foreach</h3>
+        <p>foreach($array as [$clave => SI queremos acceder a la clave] $valor)</p>
+        <?php 
+        $numeros = [1, 3, 8];
+        echo "<p><br>";
+        foreach ($numeros as $numero) {
+            echo "Elemento es $numero<br>";
+        };
+        $alumno['nombre'] = "Juan";
+        $alumno['apellido'] = "Sánchez";
+        $alumno[0] = 6;
+        $alumno[1] = 9;
+        $alumno[2] = 10;
+        $alumno['media'] = 8.25;
+
+        foreach($alumno as $clave => $valor) {
+            echo "Elemento con clave $clave y valor $valor <br>";
+        };
+
+        $componentes['cpu'] = "i7 Ultra 13th";
+        $componentes['mt'] = "Asus H81M2";
+        $componentes['ram'] = "Kingston DDR4 3200Mhz 16GB";
+        $componentes['sdd'] = "Samsung EVO 950 1 TB Nvme m.2";
+        $componentes['caja'] = "Caja con fuente 700w";
+        $componentes['monitor'] = "Monitor UHD 4K 23\"";
+        foreach($componentes as $componente) {
+            echo "El ordenador de Perico de los palotes tiene $componente<br>";
+        };
+        echo "</p>";
+        ?>
+        <h2>Funciones para arrays</h2>
+        <p>array_key_exists(clave, array) = Devuelve true si existe una clave dada en un array dado</p>
+        <p>array_search() (investigar)</p>
     </body>
 </html>
